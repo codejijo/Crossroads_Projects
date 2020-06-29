5302edfb1b32b6017c2cd47cf6aac032318741d1
Jijo John <jijojohnlikeu@gmail.com>
2020-06-29 15:33:30 +0530

    Initial Commit

---
+++ Python/GUI Calculator/Calculator_v1.5.py
@@ -0,0 +1,69 @@
+from tkinter import *
+import math
+
+calc = Tk()
+calc.geometry("300x450+350+150")
+calc.resizable(0, 0)
+calc.title("Calculator")
+
+def show(a):
+    # print(a)
+    if isinstance(a, str):
+        en.insert(END, a)
+    else:
+        en.insert(END, a.widget["text"])
+
+def clear():
+    en.delete(0, END)
+
+def operate():
+    calc.geometry("500x650+350+150")
+    value = en.get()
+    clear()
+    en.insert(END, eval(str(value)))
+
+def hoverin(a):
+    # print("kitti")
+    numbtns[int(a.widget["text"])-1].configure(background="#a8dadc")
+def hoverout(a):
+    # print("poyi")
+    numbtns[int(a.widget["text"])-1].configure(background="#f1faee")
+
+enframe = Frame(calc, width=300, height=300)
+enframe.pack(expand=True, fill="both", ipady=10)
+
+en = Entry(enframe, font=("Helvetica", 25,'bold'), background="#264653", fg="#fff",justify=RIGHT)
+en.pack(expand=True, fill="both", ipady=10)
+
+btnframe = [Frame(calc) for i in range(5)]
+for i in btnframe:
+    i.pack(expand=True, fill="both")
+
+numbtns = [Button(btnframe[math.ceil(i / 3) - 1], text=str(i), font=("Verdana", 22),
+                  relief=GROOVE, border=0,background="#f1faee", fg="#000") for i in range(1, 10)]
+for i in numbtns:
+    i.pack(side=LEFT, expand=True, fill="both")
+    i.bind('<Button-1>', show)
+    i.bind('<Enter>', hoverin)
+    i.bind('<Leave>', hoverout)
+
+btn_plus = Button(btnframe[0], text="+", command=lambda: show("+"), font=("Verdana", 22,'bold'), relief=GROOVE, border=0,background="#2a9d8f", fg="#fff")
+btn_plus.pack(side=LEFT, expand=True, fill="both", ipadx=1)
+btn_minus = Button(btnframe[1], text="-", command=lambda: show("-"), font=("Verdana", 22,'bold'), relief=GROOVE, border=0,background="#2a9d8f", fg="#fff")
+btn_minus.pack(side=LEFT, expand=True, fill="both", ipadx=6)
+btn_multi = Button(btnframe[2], text="*", command=lambda: show("*"), font=("Verdana", 22,'bold'), relief=GROOVE, border=0,background="#2a9d8f", fg="#fff")
+btn_multi.pack(side=LEFT, expand=True, fill="both", ipadx=3)
+btn_clear = Button(btnframe[3], text="C", command=clear, font=("Verdana", 22,'bold'), relief=GROOVE, border=0,background="#e63946", fg="#fff")
+btn_clear.pack(side=LEFT, expand=True, fill="both")
+btn_zero = Button(btnframe[3], text="0", command=lambda: show("0"), font=("Verdana", 22), relief=GROOVE, border=0,background="#f1faee", fg="#000")
+btn_zero.pack(side=LEFT, expand=True, fill="both", ipadx=38)
+btn_div = Button(btnframe[3], text="/", command=lambda: show("/"), font=("Verdana", 22,'bold'), relief=GROOVE, border=0,background="#2a9d8f", fg="#fff")
+btn_div.pack(side=LEFT, expand=True, fill="both", ipadx=6)
+
+btn_equal = Button(btnframe[4], text="=", command=operate, font=("Verdana", 22,'bold'), relief=GROOVE, border=0,background="#aacc00", fg="#fff")
+btn_equal.pack(side=LEFT, expand=True, fill="both",ipadx=72)
+btndot = Button(btnframe[4], text=".", command=lambda: show("."), font=("Verdana", 22,'bold'), relief=GROOVE, border=0,background="#2a9d8f", fg="#fff")
+btndot.pack(side=LEFT, expand=True, fill="both", ipadx=12)
+
+
+calc.mainloop()
---
+++ Python/GUI Calculator/test.py
@@ -0,0 +1,45 @@
+# from tkinter import *
+# import tkinter.font as font
+#
+# gui = Tk(className='Python Examples - Button')
+# gui.geometry("500x200")
+#
+# # define font
+# myFont = font.Font(family='Helvetica', size=20, weight='bold')
+#
+# # create button
+# button = Button(gui, text='My Button', bg='#0052cc', fg='#ffffff')
+# # apply font to the button label
+# button['font'] = myFont
+# # add button to gui window
+# button.pack()
+
+# gui.mainloop()
+
+# from tkinter import *
+# import tkinter.ttk as ttk
+# import tkinter.font as font
+#
+# root = Tk()
+# style = ttk.Style()
+#
+# myFont = font.Font(family='Poppins', size=10, weight='bold')
+# style.configure("TButton", padding=6, relief="flat",
+#    background="#ccc",width=8,height=2, font=myFont)
+# style.configure("BW.TLabel", foreground="black", background="white")
+#
+# l1 = ttk.Label(text="Test", style="BW.TLabel")
+# l2 = ttk.Label(text="Test", style="BW.TLabel")
+# l1.pack()
+# l2.pack()
+#
+# btn = ttk.Button(text="Sample")
+# btn.pack()
+#
+# root.mainloop()
+# import math
+# a = 9
+# print(math.ceil(a / 3))
+# li = [1,2,3,4,5]
+# print('x\u00b2')
+
